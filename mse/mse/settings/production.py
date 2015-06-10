4283e0d9cc1b6e84004862c61990abf814e60592
Don Button <donpublic@digitalgizmo.com>
2015-06-10 15:54:26 -0400

    actually add new settings files, final mse2?

---
+++ mse/mse/settings/production.py
@@ -0,0 +1,28 @@
+"""
+Local Django settings for mse project.
+"""
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mse2db',
+        'USER': 'msedb_user',
+        'PASSWORD': 'cwmorgan$1814',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+SITE_ID = 1
+
+# 3 for devel, 4 for public
+STATUS_LEVEL = 4
+# for devel vs. produciton diffes: google analytics in base.html
+IS_PRODUCTION = True
