affcd0d646f5cc6452b77e71e651aab73f7e7abf
Don Button <donpublic@digitalgizmo.com>
2015-06-11 20:50:22 -0400

    fix path for collect

--- mse/management/views.py
+++ mse/management/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.conf import settings
 
 from invoke import task, run
+from unipath import Path
 
 def index(request):
     return render_to_response('management/index.html')
@@ -12,6 +13,6 @@ def collect(request):
 	if settings.IS_PRODUCTION:
 		feedback = 'No collect possible in producion environment.'
 	else:
-		run("python manage.py collectstatic -v0 --noinput")
+		settings.BASE_DIR.child('mse', 'manage.py collectstatic -v0 --noinput')
 		feedback = 'Collectstatic has started.'
 	return render_to_response('management/collect.html', {'feedback': feedback})
