aca10eb95608060cef37d4103625ee3783de382d
Don Button <donpublic@digitalgizmo.com>
2015-06-14 14:25:00 -0400

    sent collect to usr bin

--- mse/management/views.py
+++ mse/management/views.py
@@ -13,10 +13,15 @@ def collect(request):
 	if settings.IS_PRODUCTION:
 		feedback = 'No collect possible in producion environment.'
 	else:
-		#run(settings.BASE_DIR.child('manage.py collectstatic -v0 --noinput'))
-		#run(settings.BASE_DIR.ancestor(3).child('./msesand_collect_local.sh'))
-		#run('/Users/don/Sites/msesand_collect_local.sh')
-		run('/var/www/mseadmin/data/www/msedev_collect.sh')
-		feedback = 'Collectstatic has started. eApps version'
-		#feedback = 'Collectstatic not yet functional on msedev.'
+		run('/usr/local/bin/msedev_collect.sh')
+		feedback = 'Collectstatic  started for msesand '
+	return render_to_response('management/collect.html', {'feedback': feedback})
+
+@task
+def collect_sand(request):
+	if settings.IS_PRODUCTION:
+		feedback = 'No collect possible in producion environment.'
+	else:
+		run('/usr/local/bin/msesand_collect.sh')
+		feedback = 'Collectstatic  started for msesand '
 	return render_to_response('management/collect.html', {'feedback': feedback})
