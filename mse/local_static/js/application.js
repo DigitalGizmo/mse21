7ca756e46c056bf2afd5baa1c4013efd21886918
Don Button <donpublic@digitalgizmo.com>
2015-09-16 15:09:15 -0400

    fixed menu size change at 690
    other problems remain!

--- mse/local_static/js/application.js
+++ mse/local_static/js/application.js
@@ -1,6 +1,25 @@
 // mse2 scripts
 
 $(document).ready(function(){
+
+  // resize logo as long as viewport is larger than mobile
+  $('#logo--image-img').css('height', $('#banner').height()); // "$('#banner').height()"  .logo--image
+  $(window).on('resize', function(){
+    var win = $(this); //this = window
+    $('#dimensions').text("window width: " + win.width() + "  header height: " + $('#banner').height() );
+    if (win.width() >= 690) {  
+      $('#logo--image-img').css('height', $('#banner').height()); // "$('#banner').height()"  .logo--image
+    } else {
+      $('#logo--image-img').css('height', 81); 
+    }
+  });
+
+
+  // dropdown on hover on large screen only
+
+  // find whether toggle is visible
+  // and what about on resize?
+  
   $('.dropdown').hover(
     function () {
       $('div', this).stop().slideDown(500);
@@ -13,4 +32,5 @@ $(document).ready(function(){
   $(".menu-toggle a").click(function() {
     $(".main-navigation--items").slideToggle();
   });
+
 });
