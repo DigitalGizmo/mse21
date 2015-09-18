d7fde9e7d04910b2a64ac193e2afa10b4289250f
Don Button <donpublic@digitalgizmo.com>
2015-09-18 15:39:00 -0400

    remove augmented from home page

--- mse/general/views.py
+++ mse/general/views.py
@@ -16,7 +16,7 @@ def index(request):
         project_list = Project.objects.filter(status_num__gte=settings.STATUS_LEVEL).order_by('title')
         artifact_list = Artifact.objects.filter(augmented=True, status_num__gte=settings.STATUS_LEVEL)
         document_list = Document.objects.filter(augmented=True, status_num__gte=settings.STATUS_LEVEL)
-        map_list = Geomap.objects.all().filter(augmented=True, status_num__gte=settings.STATUS_LEVEL)
+        map_list = Geomap.objects.all().filter(status_num__gte=settings.STATUS_LEVEL)
         return render_to_response('index.html', {'item_list': item_list, 'project_list': project_list, 
             'artifact_list': artifact_list, 'document_list': document_list, 'map_list': map_list})
 
