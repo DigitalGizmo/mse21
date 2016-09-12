9d9756c67e4b10e5bb0aa7fec62642159f26c0be
Don Button <donpublic@digitalgizmo.com>
2016-09-12 11:47:25 -0400

    change devel status level to 2
    Had already been changed on server, but now via git.

--- mse/mse/settings/staging.py
+++ mse/mse/settings/staging.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 SITE_ID = 1
 
-# 3 for devel, 4 for public
-STATUS_LEVEL = 3
+# 2 & 3 for devel, 4 for public
+STATUS_LEVEL = 2
 # for devel vs. produciton diffes: google analytics in base.html
 IS_PRODUCTION = False
