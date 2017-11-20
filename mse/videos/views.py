7f42df58acbadf7e0a0cc0f13c3ae50e469a0ecc
Don Button <donpublic@digitalgizmo.com>
2017-11-20 15:35:37 -0500

    initial video nav

--- mse/videos/views.py
+++ mse/videos/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.conf import settings
 from django.views.generic import ListView, DetailView
 from .models import Video
+from core.views import MenuInfoMixin
 
-class VideoListView(ListView):
+class VideoListView(MenuInfoMixin, ListView):
     # model = Video
     queryset = Video.objects.filter(status_num__gte=settings.STATUS_LEVEL)
     context_object_name = 'resource_object_list'
     # template_name = 'videos/video_list.html' 
+    menu_type='video'
 
 
-class VideoDetailView(DetailView):
+class VideoDetailView(MenuInfoMixin, DetailView):
     model = Video
     slug_field = 'short_name'
     context_object_name = 'resource_object'
     # template_name = 'videos/video_detail.html'
+    menu_type='video'
